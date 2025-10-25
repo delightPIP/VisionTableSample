9872d3c85093c0aeebf010958e2d3c26db2abd9b
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-10-25 20:53:51 +0900

    chore: sample project

---
+++ VisionTable/VisionTable/VisionTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTableApp.swift
+//  VisionTable
+//
+//  Created by taeni on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
